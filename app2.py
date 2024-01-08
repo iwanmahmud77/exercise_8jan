4121f9dcab0a2f31380ee6588785a2b4409783da
iwanmahmud77 <52195525+iwanmahmud77@users.noreply.github.com>
2024-01-08 15:32:29 +0800

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st 
+
+pickle_in = open("reg_RF.pkl","rb")
+reg_RF=pickle.load(pickle_in)
+
+
+def predict_std_perf(hour_s,pre_score):
+    prediction=reg_RF.predict([[hour_s,pre_score]])
+    print(prediction)
+    return prediction
+
+def main():
+    st.title("Student Performance Prediction")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Student Performance ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    hour_s = st.text_input("hour of study","Type Here")
+    pre_score = st.text_input("previous score","Type Here")
+    result=""
+    if st.button("Predict"):
+        result=predict_std_perf(hour_s,pre_score)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets Learn Python")
+        st.text("Built with Streamlit")
+
+if __name__ == '__main__':
+    main()
